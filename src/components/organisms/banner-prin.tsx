d5f9f0b7a78c9e75595c0ddd958f4eb9106de182
iamtuesday <inf.roycd@gmail.com>
2024-03-26 23:56:44 -0400

    feat: was added integration x2

--- src/components/organisms/banner-prin.tsx
+++ src/components/organisms/banner-prin.tsx
@@ -11,11 +11,19 @@ interface BannerPrinProps {
 }
 
 export const BannerPrin: FC<BannerPrinProps> = ({ banner }) => {
+	const formatTitle = (title: string) => {
+		return title.replace(/\//g, '<span>')
+	}
+
+	const formatDescription = (title: string) => {
+		return title.replace(/\/([^\/]+)\//g, '<span>$1</span>')
+	}
+
 	return (
 		<section className="banner-prin">
 			<div className="banner-prin__container">
 				<div className="banner-prin__content">
-					<h1 className="banner-prin__title">{banner.title}</h1>
+					<h1 className="banner-prin__title" dangerouslySetInnerHTML={{ __html: formatTitle(banner.title) }} />
 
 					<p className="banner-prin__description">{banner.subtitle}</p>
 				</div>
@@ -23,7 +31,15 @@ export const BannerPrin: FC<BannerPrinProps> = ({ banner }) => {
 				<div className="banner-prin__form">
 					<div className="banner-prin__form-content">
 						<h2 className="banner-prin__form-title">{banner.formTitle ?? 'Get your free estimate now!'}</h2>
-						<p className="banner-prin__form-description">{banner.formDescription ?? 'Get your free estimate now!'}</p>
+						<p
+							className="banner-prin__form-description"
+							dangerouslySetInnerHTML={{
+								__html: formatDescription(
+									banner.formDescription ??
+										'Fill in the form below and we will contact you within 24 hours to get your free estimate.'
+								)
+							}}
+						/>
 					</div>
 					<FormPrin />
 				</div>
